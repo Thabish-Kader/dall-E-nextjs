324ba25e29da957443aa608ba7cdbe810aec41e3
Thabish <kadertabish2@gmail.com>
2023-03-19 14:36:06 +0400

    communitypost
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,37 +1,38 @@
 import { Gallery } from "@/components/Gallery";
 
-// async function getData() {
-// 	const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`, {
-// 		next: { revalidate: 120 },
-// 	});
+async function getPosts() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`, {
+		next: { revalidate: 120 },
+	});
 
-// 	if (!res.ok) {
-// 		throw new Error("Failed to fetch data");
-// 	}
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
 
-// 	return res.json();
-// }
+	return res.json();
+}
 
 const Share = async () => {
-	// const { posts } = await getData();
+	const { posts } = await getPosts();
 	// DUMMY DATA
-	const posts = [
-		{
-			postId: "1",
-			title: "night",
-			imageUrl:
-				"https://images.nightcafe.studio/jobs/nuha3SGmXAZQKkZt6QwJ/nuha3SGmXAZQKkZt6QwJ.jpg?tr=w-1600,c-at_max",
-			tags: ["#night", "#ai"],
-		},
-		{
-			postId: "2",
-			title: "greenLantern",
-			imageUrl:
-				"https://davidwaltonfiction.files.wordpress.com/2022/10/2979412993_young_handsome_asian_man_dinosaur_bones_in_green_smoke__head_and_shoulders_portrait__8k_resolution_concept_art_portrait_by_greg_rutkowski__artgerm__wlop__alphonse_mucha_dynami.png",
-			tags: ["#green", "#lantern"],
-		},
-	];
+	// const posts = [
+	// 	{
+	// 		postId: "1",
+	// 		title: "night",
+	// 		imageUrl:
+	// 			"https://images.nightcafe.studio/jobs/nuha3SGmXAZQKkZt6QwJ/nuha3SGmXAZQKkZt6QwJ.jpg?tr=w-1600,c-at_max",
+	// 		tags: ["#night", "#ai"],
+	// 	},
+	// 	{
+	// 		postId: "2",
+	// 		title: "greenLantern",
+	// 		imageUrl:
+	// 			"https://davidwaltonfiction.files.wordpress.com/2022/10/2979412993_young_handsome_asian_man_dinosaur_bones_in_green_smoke__head_and_shoulders_portrait__8k_resolution_concept_art_portrait_by_greg_rutkowski__artgerm__wlop__alphonse_mucha_dynami.png",
+	// 		tags: ["#green", "#lantern"],
+	// 	},
+	// ];
 
+	console.log(posts);
 	return (
 		<main className="max-w-5xl mx-auto">
 			{/*  */}
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,20 +2,44 @@ import { Post } from "@prisma/client";
 import Image from "next/image";
 import React, { FC } from "react";
 
-const CommunityPost: FC<Post> = ({ title, imageUrl, tags }) => {
+const CommunityPost: FC<Post> = ({
+	title,
+	author,
+	authorImage,
+	imageUrl,
+	tag,
+}) => {
 	return (
-		<article className="bg-openAI_Primary flex flex-col p-5 rounded-lg">
-			<div className="relative h-[512px] w-[512px]">
-				<Image src={imageUrl} alt={title} fill />
+		<article className="bg-openAI_Primary flex flex-col p-5 rounded-lg text-white">
+			<div className="relative h-[512px] w-auto rounded-lg">
+				<Image
+					src={imageUrl}
+					alt={title}
+					fill
+					className="object-contain "
+				/>
 			</div>
-			<h1 className="text-2xl text-white uppercase font-bold tracking-widest">
+			<h1 className="text-2xl text-white uppercase font-bold tracking-widest sm:mt-2">
 				{title}
 			</h1>
-			<div className="flex items-center gap-2">
-				{tags.map((tag, i) => (
-					<span key={i}>{tag}</span>
-				))}
+			{/* user profile/name */}
+			<div className="flex items-center space-x-3">
+				<div className="relative h-12 w-12 ">
+					<Image
+						src={authorImage}
+						alt={title}
+						fill
+						className="object-cover rounded-full"
+					/>
+				</div>
+				<p>{author}</p>
 			</div>
+			<p className="text-yellow-500 mt-2">
+				# Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Repellat error vero cumque, sed tempora suscipit facilis. Dolore
+				quo saepe aut laboriosam, veritatis officia. Neque doloribus
+				nostrum aliquam error eos perspiciatis!
+			</p>
 		</article>
 	);
 };
@@ -23,8 +47,10 @@ const CommunityPost: FC<Post> = ({ title, imageUrl, tags }) => {
 export const Gallery: FC<GalleryProps> = ({ posts }) => {
 	return (
 		<section className="mt-40">
-			<h1 className="text-white">Community Showcase</h1>
-			<div className="grid  gap-2">
+			<h1 className="text-white text-5xl my-4 underline">
+				Community Showcase
+			</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
 				{posts.map((post) => (
 					<CommunityPost key={post.id} {...post} />
 				))}
