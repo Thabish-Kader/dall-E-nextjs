a621fa791fce0e7024d6e685804e4a9e34b4a0ff
Thabish <kadertabish2@gmail.com>
2023-03-20 08:24:36 +0400

    bug-fix
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -2,18 +2,18 @@ import { Gallery } from "@/components/Gallery";
 import { prisma } from "@/prisma/prisma";
 
 async function getPosts() {
-	// const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`);
-	// if (!res.ok) {
-	// 	throw new Error("Failed to fetch data");
-	// }
-	// const posts = res.json();
-	const posts = await prisma.post.findMany();
+	const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`);
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+	const posts = res.json();
+	// const posts = await prisma.post.findMany();
 
 	return posts;
 }
 
 const Share = async () => {
-	const posts = await getPosts();
+	const { posts } = await getPosts();
 
 	return (
 		<main className="max-w-5xl mx-auto">
